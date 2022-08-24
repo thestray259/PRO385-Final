10d26a193816dcdd866a88cfc192297168bbccb6
Tobie Allen <tallen@student.neumont.edu>
2022-08-24 14:20:18 -0600

    added some obstacles and worked more on comp movement

--- PRO385/Assets/Scripts/CompanionAI/Companion.cs
+++ PRO385/Assets/Scripts/CompanionAI/Companion.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 public class Companion : Agent
 {
     [SerializeField] GameObject player;
+    [SerializeField] NavMeshMovement navMeshMovement; 
     //[SerializeField] CharacterController companionController; 
 
     public float speed = 5; 
     public FloatRef playerDistance;
-    public Vector3 destination; 
+    public Vector3 destination;
+    public Quaternion rotation;
+    public Vector3 forward; 
 
     void Start()
     {
@@ -25,15 +28,30 @@ public class Companion : Agent
     void Move()
     {
         // it's spinning on the y a bunch???
+        
+        //Vector3 destination = transform.position + forward * Random.Range(10f, 15f);
 
         playerDistance.value = Vector3.Distance(this.transform.position, player.transform.position);
 
         if (playerDistance.value >= 2.5f) // if player is too far away from companion 
         {
             // move towards player
-            destination = player.transform.position;// + new Vector3(1, 0, 1);
-            movement.MoveTowards(destination);
-            //if (playerDistance.value! >= 2.5f) movement.Stop(); 
+            rotation = Quaternion.AngleAxis(Random.Range(-90, 90), Vector3.up);
+            forward = rotation * Vector3.forward;
+
+            destination = player.transform.position + forward;// * Random.Range(1f, 3f);// + new Vector3(1, 0, 1);
+            if (navMeshMovement.navMeshAgent.isStopped == true)
+            {
+                movement.Resume();
+                //movement.MoveTowards(destination);
+            }
+            else movement.MoveTowards(destination); 
+            
+            if (playerDistance.value <= 2.4f)
+            {
+                movement.Stop(); 
+            }
+
         }
         //else movement.Stop();
     }
